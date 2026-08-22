b1374a12af93709ecd56b31cb14966edb7928a6e
Arthur Cheng <arthurcheng@Mac.modem>
2026-08-22 11:38:27 +1000

    Added ARKit Xcode project skeleton + gitignore

---
+++ LASAL/LASAL/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LASAL
+//
+//  Created by Atharv Singh Panwar on 22/8/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LASAL/LASAL/LASALApp.swift
@@ -0,0 +1,17 @@
+//
+//  LASALApp.swift
+//  LASAL
+//
+//  Created by Arthur Cheng on 22/8/2026.
+//
+
+import SwiftUI
+
+@main
+struct LASALApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
